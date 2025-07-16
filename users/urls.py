5e0bb510191765594a3576592e84784babd1aec7
kimmich14 <Kimathifrank10@gmail.com>
2025-07-16 22:37:12 +0300

    Add Password Reset

--- users/urls.py
+++ users/urls.py
@@ -12,9 +12,20 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'), # Leaners' dashboard. Shows all their interaction and learning activities
     path('instructor/', views.instructor_dashboard, name='instructor_dashboard'), # Tutors' dashboard. Shows all their content with CRUD functionalities
     path('redirect/', views.role_redirect_view, name='role_redirect'), # Redirects learnears and tutors to their respective dashboards
-    path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='auth/password_reset.html',
-        email_template_name='auth/password_reset_email.html',
-        subject_template_name='auth/password_reset_subject.txt'
-    ), name='password_reset'), # Users' password management
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='auth/password_reset.html',
+             email_template_name='auth/password_reset_email.html',
+             subject_template_name='auth/password_reset_subject.txt'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
